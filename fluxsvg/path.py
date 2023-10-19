3d02fd4f53d46b0761cd26ad597116ff15d369d9
Dean Sung <dean@flux3dp.com>
2023-10-19 12:40:36 +0800

    Fix path T after t or q

--- fluxsvg/path.py
+++ fluxsvg/path.py
@@ -448,8 +448,8 @@ def path(surface, node):
             if last_letter not in 'QqTt':
                 x2, y2, x3, y3 = abs_x, abs_y, abs_x, abs_y
             elif last_letter in 'qt':
-                x2 += abs_x
-                y2 += abs_y
+                x2 += (abs_x - x3)
+                y2 += (abs_y - y3)
             x2 = 2 * abs_x - x2
             y2 = 2 * abs_y - y2
             x1, y1 = abs_x, abs_y
