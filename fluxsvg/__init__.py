da4b0cda2e7700c72d27db9968c20ecb0cbeaead
Dean Sung <dean@flux3dp.com>
2024-07-01 17:14:15 +0800

    2.7.6

--- fluxsvg/__init__.py
+++ fluxsvg/__init__.py
@@ -20,7 +20,7 @@ CairoSVG - A simple SVG converter based on Cairo.
 """
 
 # Handle by layer svg id
-__version__ = '2.7.5'  # noqa (version is used by relative imports)
+__version__ = '2.7.6'  # noqa (version is used by relative imports)
 
 
 import os
